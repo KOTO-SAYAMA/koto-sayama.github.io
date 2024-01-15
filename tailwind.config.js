f9e2fa0cc5286141a568120c754554cdfbfc9236
sato_uwu <156490035+KOTO-SAYAMA@users.noreply.github.com>
2024-01-15 23:33:56 +1300

    Add font and update designs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dotGothic: ["DotGothic16","sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
